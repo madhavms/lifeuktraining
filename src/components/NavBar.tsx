d231c45b499ba6a2eff87b236490c1b3cb7b07fc
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-07-19 13:46:04 +0100

    Navbar style corrections

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,10 +10,22 @@ import {
   ListItemText,
   useMediaQuery,
 } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { MuiThemeProvider, createMuiTheme, makeStyles, useTheme } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import { AiOutlineDown } from "react-icons/ai";
 import { MenuContent, NavBarLogoContent } from "../assets/NavBarMenuContent";
+
+
+const themeMUI = createMuiTheme({
+  props: {
+    // Name of the component
+    MuiButtonBase: {
+      // The properties to apply
+      disableRipple: true // No more ripple, on the whole application!
+    }
+  }
+});
+
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -31,12 +43,26 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     gap: "1rem",
-    opacity: "0.9",
     [theme.breakpoints.down("md")]: {
       flexBasis: "100%",
       justifyContent: "flex-start",
       gap: "0",
     },
+    disableRipple: true
+  },
+  listItem: {
+    opacity: "1",
+    transition: "opacity 0.3s ease-in-out",
+    "&:hover": {
+      opacity: 0.5,
+      backgroundColor: "transparent",
+    },
+    disableRipple: true
+  },
+  listItemImage: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    }
   },
   listItemText: {
     whiteSpace: "nowrap",
@@ -54,11 +80,11 @@ const Navbar = () => {
   };
 
   return (
-    <>
+    <MuiThemeProvider theme={themeMUI}>
       <div className="flex flex-col md:flex-row justify-center items-center md:justify-between m-5">
         {NavBarLogoContent.map((item) => (
           <div key={item.id}>
-            <ListItem button component={Link} to={item.linkTo}>
+            <ListItem className={classes.listItemImage} button component={Link} to={item.linkTo}>
               <img className="h-20 w-60" src={item.src} alt={item.alt} />
             </ListItem>
           </div>
@@ -86,6 +112,7 @@ const Navbar = () => {
                       component={Link}
                       to={item.link}
                       key={item.id}
+                      className={classes.listItem}
                     >
                       <ListItemText
                         primary={item.title}
@@ -103,7 +130,7 @@ const Navbar = () => {
           <Drawer anchor="left" open={isOpen} onClose={toggleNavbar}>
             <List>
               {MenuContent.map((item) => (
-                <ListItem button component={Link} to={item.link} key={item.id}>
+                <ListItem  className={classes.listItem} button component={Link} to={item.link} key={item.id}>
                   <ListItemText
                     primary={item.title}
                     classes={{ primary: classes.listItemText }}
@@ -115,7 +142,7 @@ const Navbar = () => {
           </Drawer>
         )}
       </div>
-    </>
+    </MuiThemeProvider>
   );
 };
 
